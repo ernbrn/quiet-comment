e0129f66f54dd13570fd703ca4930441666ec365
Erin Brown <emberbrown@gmail.com>
2018-02-02 14:34:22 -0500

    Creating a test pr branch

--- event.js
+++ event.js
@@ -2,3 +2,4 @@ chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
   chrome.tabs.executeScript(null, {file: "script.js"});
 }, {url: [{hostContains: 'github.', urlContains: 'pull', urlSuffix: 'files'}]});
 
+
