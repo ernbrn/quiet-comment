006b13e4e8b71c96ef7033e5adb97cdf5c07663e
Erin Brown <emberbrown@gmail.com>
2018-02-02 13:15:44 -0500

    Create the button and have it toggle all the comments when pressed

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-alert('HELLO YAY');
+if (!$('#quiet-comment-button-container').length) {
+  var classToggleName = 'comments-quiet';
+  var $prReveiewTools = $('.pr-review-tools');
+
+  var $buttonContainer = $('<div class="diffbar-item" id="quiet-comment-button-container"></div>');
+  var $button = $('<button type="button" class="btn btn-sm btn-success js-menu-target" aria-expanded="false" aria-haspopup="true"></button>');
+  var setButtonText = function() {
+    var buttonText = $button.hasClass(classToggleName) ? 'Show Comments' : 'Quiet Comments';
+    $button.text(buttonText)
+  };
+
+  setButtonText();
+
+  $button.appendTo($buttonContainer);
+  $buttonContainer.prependTo($prReveiewTools);
+
+  $button.click(function() {
+    var elements = document.getElementsByClassName('js-toggle-file-notes');
+    [].forEach.call(elements, (el) => el.click());
+    $button.toggleClass(classToggleName);
+    setButtonText();
+  });
+}
+
